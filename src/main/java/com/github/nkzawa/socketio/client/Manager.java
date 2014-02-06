0a3a3b33a1b308246c333084f5ff300c9433891b
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-02-07 03:47:15 +0900

    make Engine an inner class

--- src/main/java/com/github/nkzawa/socketio/client/Manager.java
+++ src/main/java/com/github/nkzawa/socketio/client/Manager.java
@@ -385,4 +385,24 @@ public class Manager extends Emitter {
 
         public void call(Exception err);
     }
+
+
+    private class Engine extends com.github.nkzawa.engineio.client.Socket {
+
+        Engine(URI uri, Options opts) {
+            super(uri, opts);
+        }
+
+        @Override
+        public void onopen() {}
+
+        @Override
+        public void onmessage(String s) {}
+
+        @Override
+        public void onclose() {}
+
+        @Override
+        public void onerror(Exception err) {}
+    }
 }
