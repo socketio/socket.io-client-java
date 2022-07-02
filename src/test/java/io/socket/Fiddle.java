891bea342f15caf686e5d548996021e035db858a
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2022-07-02 07:24:33 +0200

    test: add test class (2)
    Let's use Java 7 syntax for now.

--- src/test/java/io/socket/Fiddle.java
+++ src/test/java/io/socket/Fiddle.java
@@ -1,20 +1,10 @@
 package io.socket;
 
 import io.socket.client.IO;
-import io.socket.client.Manager;
 import io.socket.client.Socket;
-import okhttp3.OkHttpClient;
+import io.socket.emitter.Emitter;
 
-import javax.net.ssl.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.URI;
-import java.security.*;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.concurrent.ExecutorService;
 
 public class Fiddle {
 
@@ -23,16 +13,25 @@ public class Fiddle {
 
         Socket socket = IO.socket(URI.create("http://localhost:3000"), options);
 
-        socket.on(Socket.EVENT_CONNECT, (__) -> {
-            System.out.println("connect");
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                System.out.println("connect");
+            }
         });
 
-        socket.on(Socket.EVENT_CONNECT_ERROR, (args) -> {
-            System.out.println("connect_error: " + args[0]);
+        socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                System.out.println("connect_error: " + args[0]);
+            }
         });
 
-        socket.on(Socket.EVENT_DISCONNECT, (args) -> {
-            System.out.println("disconnect due to: " + args[0]);
+        socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                System.out.println("disconnect due to: " + args[0]);
+            }
         });
 
         socket.connect();
