c102ae5f3401164de1c5f3541d829fe0ca7fe1d2
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2024-07-10 12:55:30 +0200

    update site
    - version 2.1.1

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.socket.backo"},{"l":"io.socket.client"},{"l":"io.socket.hasbinary"},{"l":"io.socket.parser"}];updateSearchResults();
